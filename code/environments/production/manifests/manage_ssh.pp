a9fac95690266efcb256d337fb00bb01f80beb9a
Pierre <psable@dawan.fr>
2021-07-26 17:13:33 +0200

    Validation TP ordring

---
+++ code/environments/production/manifests/manage_ssh.pp
@@ -0,0 +1,28 @@
+# Resource file_line => gestion de contenu de fichier
+package { 'openssh-server':
+  ensure => 'installed',
+}
+
+file_line { 'conf sshd':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PermitRootLogin no',
+  match  => '^PermitRootLogin .*',
+  notify => Service['gestion service ssh'],
+}
+
+#augeas { 'conf sshd':
+#    context => '/files/etc/ssh/sshd_config',
+#    changes => [ 'set PermitRootLogin no',
+#                 'set LogLevel DEBUG',
+#               ],
+#    notify  => Service['gestion service ssh'],
+#    require => Package['openssh-server'],
+#}
+
+service { 'gestion service ssh':
+  ensure  => 'running',
+  name    => 'sshd',
+  enable  => 'true',
+  require => Package['openssh-server'],
+}
