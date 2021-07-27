a7482a0ba355d67cf955026bed632a3e52b83701
Pierre <psable@dawan.fr>
2021-07-27 15:27:01 +0200

    Pouvoir réutiliser une classe dans le meme catalogue : transformation define

---
+++ code/environments/production/manifests/demo_define_class.pp
@@ -0,0 +1,6 @@
+# Classe standard
+define demo_define ( String $mon_message = $title )
+{
+  notify { $mon_message:
+  }
+}
--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -4,6 +4,8 @@ node default {
   include manage_user
   include manage_ntp
   include manage_ssh
+  include manage_tz
+
   class { 'class_param':
     mon_parametre => 'common param'
   }
@@ -21,4 +23,9 @@ node 'cli01.formation.lan' {
   class { 'manage_tz':
     timezone => 'Europe/Madrid'
   }
+
+  demo_define {'Instance bdd01':}
+
+  demo_define {'Instance bdd02':}
+
 }
