ffe889bd24e6e8674fabdc02865169638aa7b10f
Pierre <psable@dawan.fr>
2021-07-28 16:16:32 +0200

    Changement methode node classifier : avec appel par hiera

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -1,10 +1,12 @@
 # Declaration de classe
 
-node default {
-  include role::base
-}
+#node default {
+#  include role::base
+#}
+#
+#node 'cli01.formation.lan', 'cli03.formation.lan' {
+#  # appeler un role
+#  include role::websrv
+#}
 
-node 'cli01.formation.lan' {
-  # appeler un role
-  include role::websrv
-}
+hiera_include('classes')
